5e411bac5c0997e698d045d52a5871b766bb01d3
Aristófanes Lira <aristofanes.lira@gmail.com>
2023-07-11 13:59:27 -0300

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>DevSteam: A sua loja online de games</title>
+        <meta name="description" content="DevSteam: A sua loja online de games" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        DevStart: A sua loja online de games
+      </div>
+    </>
+  )
+}
